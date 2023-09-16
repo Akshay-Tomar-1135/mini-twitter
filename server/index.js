f54dd5a829faaba220bef609729a188ac8e5dab6
Akshay-Tomar-1135 <akshaytomar1135@gmai.com>
2023-09-16 17:13:33 +0530

    added profile pic

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
+import imageRoutes from "./routes/image.js";
 
 import cors from 'cors';
 
@@ -36,6 +37,7 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
+app.use("/api/image", imageRoutes);
 
 app.listen(8000, () => {
   connect();
